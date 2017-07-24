e6209208f7aa07739dfe5b80ad85f019e2bdedf0
Alan McGovern <alan.mcgovern@gmail.com>
2017-07-24 11:38:32 +0100

    Only emit a notification if we create an accessibility element
    We should only invoke `ac_element_notify` if we actually created
    an element. The very first thing that `ac_element_notify` does is
    invoke `ac_element_get_accessibility_element`, so if that function
    cannot create an element we do not want to trigger an infinite loop.
    
    Fixes https://bugzilla.xamarin.com/show_bug.cgi?id=58310

--- AtkCocoa/acelement.c
+++ AtkCocoa/acelement.c
@@ -318,7 +318,8 @@ ac_element_get_accessibility_element (AcElement *element)
 		klass = AC_ELEMENT_GET_CLASS (element);
 		if (klass->get_accessibility_element) {
 			element->priv->real_element = (__bridge_retained void *) klass->get_accessibility_element (element);
-            ac_element_notify(element, NSAccessibilityCreatedNotification, nil);
+			if (element->priv->real_element)
+				ac_element_notify(element, NSAccessibilityCreatedNotification, nil);
 		} else {
 			g_warning ("No accessibility element method for %s", G_OBJECT_TYPE_NAME (element));
 		}
@@ -819,9 +820,11 @@ ac_element_notify (AcElement *element,
 				   NSDictionary *userInfo)
 {
 	id realElement = ac_element_get_accessibility_element (element);
-	if (userInfo) {
-		NSAccessibilityPostNotificationWithUserInfo (realElement, notificationName, userInfo);
-	} else {
-		NSAccessibilityPostNotification (realElement, notificationName);
+	if (realElement) {
+		if (userInfo) {
+			NSAccessibilityPostNotificationWithUserInfo (realElement, notificationName, userInfo);
+		} else {
+			NSAccessibilityPostNotification (realElement, notificationName);
+		}
 	}
 }
