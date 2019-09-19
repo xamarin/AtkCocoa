5865adb63c36f373ca108395718d6fccfd807f52
iain holmes <iain@xamarin.com>
2019-09-19 14:49:20 +0100

    [GtkSpinButton] Show the spin button as an entry and a stepper
    Fixes VSTS #754815

--- AtkCocoa/ACAccessibilitySpinnerElement.c
+++ AtkCocoa/ACAccessibilitySpinnerElement.c
@@ -21,13 +21,122 @@
 #include "atk-cocoa/acelement.h"
 #include "atk-cocoa/acdebug.h"
 
+@interface ACAccessibilityStepperButtonElement : NSAccessibilityElement <NSAccessibilityButton>
+
+@property (readwrite) SEL action;
+@property (readwrite, weak) id target;
+
+@end
+
+@implementation ACAccessibilityStepperButtonElement
+
+@synthesize action;
+@synthesize target;
+
+- (BOOL)accessibilityPerformPress
+{
+    if (action == nil && target == nil) {
+        return NO;
+    }
+
+    [target performSelector:action];
+    return YES;
+}
+
+@end
+@interface ACAccessibilityStepperElement : NSAccessibilityElement <NSAccessibilityStepper>
+@end
+
+@implementation ACAccessibilityStepperElement {
+    GtkWidget *_spinButton;
+    ACAccessibilityStepperButtonElement *_incrementButton;
+    ACAccessibilityStepperButtonElement *_decrementButton;
+}
+
+- (instancetype)initWithSpinButton:(GtkSpinButton *)button
+{
+    self = [super init];
+    if (self == nil) {
+        return nil;
+    }
+
+    _incrementButton = [[ACAccessibilityStepperButtonElement alloc] init];
+    [_incrementButton setAccessibilityRole:NSAccessibilityButtonRole];
+    [_incrementButton setAccessibilitySubrole:NSAccessibilityIncrementArrowSubrole];
+    [_incrementButton setTarget:self];
+    [_incrementButton setAction:@selector(accessibilityPerformIncrement)];
+    [self setAccessibilityIncrementButton:_incrementButton];
+
+    _decrementButton = [[ACAccessibilityStepperButtonElement alloc] init];
+    [_decrementButton setAccessibilityRole:NSAccessibilityButtonRole];
+    [_decrementButton setAccessibilitySubrole:NSAccessibilityDecrementArrowSubrole];
+    [_decrementButton setTarget:self];
+    [_decrementButton setAction:@selector(accessibilityPerformDecrement)];
+    [self setAccessibilityDecrementButton:_decrementButton];
+
+    _spinButton = (GtkWidget *) button;
+
+    return self;
+}
+
+- (NSArray *)accessibilityChildren
+{
+    return @[_incrementButton, _decrementButton];
+}
+
+- (void)adjustValueInDirection:(GtkSpinType)direction
+{
+    gtk_spin_button_spin (GTK_SPIN_BUTTON (_spinButton), direction, 1);
+}
+
+- (BOOL)accessibilityPerformDecrement
+{
+    [self adjustValueInDirection:GTK_SPIN_STEP_BACKWARD];
+    return YES;
+}
+
+- (BOOL)accessibilityPerformIncrement
+{
+    [self adjustValueInDirection:GTK_SPIN_STEP_FORWARD];
+    return YES;
+}
+
+#define STEPPER_WIDTH 16 // Don't think there's a way to get the stepper width, so hard code something so VO works
+- (NSRect)accessibilityFrameInParentSpace
+{
+    return NSMakeRect(_spinButton->allocation.width - STEPPER_WIDTH, 0, STEPPER_WIDTH, _spinButton->allocation.height);
+}
+
+@end
+
 @implementation ACAccessibilitySpinnerElement {
+    ACAccessibilityStepperElement *_stepper;
+    ACAccessibilityTextFieldElement *_textField;
 }
 
 - (instancetype)initWithDelegate:(AcElement *)delegate
 {
-	[self setAccessibilityRole:NSAccessibilityIncrementorRole];
-	return [super initWithDelegate:delegate];
+    self = [super initWithDelegate:delegate];
+    if (self == nil) {
+        return nil;
+    }
+
+	[self setAccessibilityRole:NSAccessibilityGroupRole];
+
+    _stepper = [[ACAccessibilityStepperElement alloc] initWithSpinButton:GTK_SPIN_BUTTON (ac_element_get_owner(delegate))];
+    _textField = [[ACAccessibilityTextFieldElement alloc] initWithDelegate:delegate];
+
+    return self;
+}
+
+- (BOOL)hasDynamicChildren
+{
+    return YES;
+}
+
+- (NSArray *)accessibilityChildren
+{
+    return @[_textField, _stepper];
 }
 
 - (GtkSpinButton *)owner
@@ -77,25 +186,4 @@
 	gtk_spin_button_set_value (owner, [value doubleValue]);
 }
 
-- (void)adjustValueInDirection:(GtkSpinType)direction
-{
-	GtkSpinButton *owner = [self owner];
-	if (owner == NULL) {
-		return;
-	}
-	
-	gtk_spin_button_spin (GTK_SPIN_BUTTON (owner), direction, 1);
-}
-
-- (BOOL)accessibilityPerformDecrement
-{
-	[self adjustValueInDirection:GTK_SPIN_STEP_BACKWARD];
-	return YES;
-}
-
-- (BOOL)accessibilityPerformIncrement
-{
-	[self adjustValueInDirection:GTK_SPIN_STEP_FORWARD];
-	return YES;
-}
 @end
